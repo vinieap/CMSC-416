71c969a9a162b65a517c8149de615585d1fae51c
Vinit Patel <vinieap@gmail.com>
2021-02-06 21:26:12 -0500

    Something

--- PA/1/eliza.py
+++ PA/1/eliza.py
@@ -1,15 +1,33 @@
 import re
 
-stop_words= "a,about,above,after,again,against,all,am,an,and,any,are,aren't,as,at,be,because,been,before,being,below,between,both,but,by,can't,cannot,could,couldn't,did,didn't,do,does,doesn't,doing,don't,down,during,each,few,for,from,further,had,hadn't,has,hasn't,have,haven't,having,he,he'd,he'll,he's,her,here,here's,hers,herself,him,himself,his,how,how's,i,i'd,i'll,i'm,i've,if,in,into,is,isn't,it,it's,its,itself,let's,me,more,most,mustn't,my,myself,no,nor,not,of,off,on,once,only,or,other,ought,our,ours,ourselves,out,over,own,same,shan't,she,she'd,she'll,she's,should,shouldn't,so,some,such,than,that,that's,the,their,theirs,them,themselves,then,there,there's,these,they,they'd,they'll,they're,they've,this,those,through,to,too,under,until,up,very,was,wasn't,we,we'd,we'll,we're,we've,were,weren't,what,what's,when,when's,where,where's,which,while,who,who's,whom,why,why's,with,won't,would,wouldn't,you,you'd,you'll,you're,you've,your,yours,yourself,yourselves".split(",")
+name = None
 
-if __name__ == '__main__':
+interrogative_words = ['what', 'when', 'why', 'which', 'who', 'how', 'whose', 'Whom', 'where']
+
+def intro():
+    global name
+
     intro = input(
         "Hi, I'm a psychotheraphist. What is your name?\n").split(' ')
     r = re.compile('[A-Z]')
     name = re.sub(r'[^\w\s]', '', list(filter(r.match, intro))[-1])
 
+def parse_question(question):
+    print(f'That is a question')
+
+def parse_answer(query):
+    query = query.lower().split(' ')
+
+    if query[0] in interrogative_words or query[-1][-1] == '?':
+        parse_question(query)
+
+def main():
+    intro()
+
     query = input(f"Hi {name}, How can I help you today?\n")
 
-    print(name)
+    while True:
+        parse_answer(query)
 
-    print(stop_words)
+if __name__ == '__main__':
+    main()
