ed79832a98bf546eabfa6395d832b7ac56ec0026
Vinit Patel <vinieap@gmail.com>
2021-03-16 17:26:52 -0400

    Finished PA3

--- PA/3/scorer.py
+++ PA/3/scorer.py
@@ -1,64 +1,177 @@
+'''
+Author: Vinit Patel
+Class: CMSC-416-001-2021Spring
+Date: 03/16/2021
+
+------
+Scorer
+------
+
+===========
+How to run:
+===========
+
+$ python scorer.py pos-test-with-tags.txt pos-test-key.txt > pos-tagging-report.txt
+
+===========
+
+Usage:
+------
+The scorer will take in the predicted tags and the actual tags and
+compare them and give an accuracy and confusion matrix between the two
+into the output file specified
+
+**********
+Algorithm:
+**********
+
+1. Read in the predictions and key file paths
+2. Read in the predictions file and turn into a list of (word, tag) tuples
+3. Read in the key file and use the tagger.py function get_tags
+	to get a list of (word, tag) tuples
+4. Simultaneously go through each list and compare their values
+	4.1 If they are not equal then add 1 to the incorrect variable
+5. Output accuracy using the incorrect and total variables
+6. Turn the lists into pandas.Series for confusion matrix
+7. Use pandas.crosstab to generate the confusion matrix
+8. Output confusion matrix
+
+Baseline (Most Likely):
+***********************
+Correct: 47,243
+Incorrect: 9,581
+Total: 56,824
+
+Accuracy: 83.14%
+
+After 5 Rules and Most Likely:
+******************************
+Correct: 48,500
+Incorrect: 8,324
+Total: 56,824
+
+Accuracy: 85.35%
+
+=======
+Results
+=======
++2.21% Increase in accuracy
+1,257 More Correct using rules
+'''
+
 import sys
-import re
-import json
 import tagger
+import pandas as pd
 
 
-def calcAcc(predictions, key):
+
+
+# Information about author and program
+def metaInfo():
+    print("Author: Vinit Patel")
+    print("Class: CMSC-416-001-2021Spring")
+    print("Date: 03/16/2021")
+    print("Tagger and Scorer")
+    print("========================")
+
+def calcAcc(predictions, keys):
+	# Store number of inaccuracies and total number of predictions
 	incorrect = 0
 	total = 0
 
-	biggest_inaccs = {}
+	# Loop through predictions and keys simultaneously
+	for p, k in zip(predictions, keys):
 
-	for word, prediction in predictions.items():
-		for tag, count in prediction.items():
-			total += count
-			correctTag = tag
+		# If the tags are not the same then add to incorrect variable
+		if p[1] != k[1]:
+			incorrect += 1
 
-		for tag, count in key[word].items():
-			if tag != correctTag:
-				if count > 50:
-					biggest_inaccs[word] = key[word]
-				incorrect += count
+		# Always add to total variable
+		total += 1
 
-	print(f"Incorrect: {incorrect}, Total: {total}")
+
+	# Output Statistics
+	print(f"Correct: {total - incorrect}")
+	print(f"Incorrect: {incorrect}")
+	print(f"-----------------")
+	print(f"Total: {total}\n")
+
 	print(f"Accuracy: {format(((total - incorrect) / total * 100), '0.2f')}%")
-	print(f"Biggest Inaccuracies:")
-	print(json.dumps(biggest_inaccs, indent=2))
+	print(f"-----------------")
+	
+
+def generateConfusionMatrix(predictions, keys):
+	# Needed to disable truncation
+	pd.set_option('display.expand_frame_repr', False)
+
+	# Get only tags
+	pred = [p[1] for p in predictions]
+	act = [k[1] for k in keys]
+
+	# List -> Series
+	actual = pd.Series(act, name='Actual')
+	predicted = pd.Series(pred, name='Predictions')
+
+	# Create the confusion matrix
+	confusion_matrix = pd.crosstab(predicted, actual)
+
+	# Output confusion matrix to file
+	print(f"Confusion Matrix:")
+	print(confusion_matrix)
 
 
 def parseKeyFile(keyFilePath):
+
+	# Read the keys file
 	with open(keyFilePath, 'r') as f:
 		lines = f.readlines()
 
-	wt_dict, word_dict, tag_dict = tagger.get_tags(lines)
+	# Reuse the function in tagger.py to read in key file
+	_, _, keys= tagger.get_tags(lines)
 
-	return wt_dict
+	return keys
 
 
 def getPredictions(predictionsFilePath):
-	with open(predictionsFilePath, 'r') as f:
-		predictions = f.read()
 
-	return eval(predictions)
+	# Read the predictions file
+	with open(predictionsFilePath, 'r') as f:
+		lines = f.readlines()
+
+	# Process each line into individual elements into a list
+	predictions = [tagger.split_tag(line.strip()) for line in lines]
+
+	return predictions
 
 
 def parseParameters():
     argv = sys.argv
 
+    # Make sure there are exactly 3 arguments
     if len(argv) != 3:
         print("Run Command Example: python scorer.py pos-test-with-tags.txt pos-test-key.txt > pos-tagging-report.txt")
         sys.exit(1)
 
+     # Returns predictions and keys file paths
     return argv[1], argv[2]
 
 
 def main():
+	# Print information about author and program
+	metaInfo()
+	
+	# Get the file paths for the predictions from the test file and the key
 	predictionsFilePath, keyFilePath = parseParameters()
-	predictions = getPredictions(predictionsFilePath)
-	wt_dict = parseKeyFile(keyFilePath)
 
-	calcAcc(predictions, wt_dict)
+	# Get the predictions and keys from their respective file
+	predictions = getPredictions(predictionsFilePath)
+	keys = parseKeyFile(keyFilePath)
+
+	# Calculate the accuracy of the predictions using the keys
+	calcAcc(predictions, keys)
+
+	# Create the confusion matrix of the key and predictions
+	generateConfusionMatrix(predictions, keys)
 
 	# print(wt_dict)
 
--- PA/3/tagger.py
+++ PA/3/tagger.py
@@ -1,149 +1,373 @@
+'''
+Author: Vinit Patel
+Class: CMSC-416-001-2021Spring
+Date: 03/16/2021
+
+------
+Tagger
+------
+
+The program reads in a training file of tagged (part of speech) words and uses the most likely
+probability tag along with some rules to predict any untagged words.
+
+===========
+How to run:
+===========
+
+-----------
+First step:
+-----------
+
+$ python tagger.py pos-train.txt pos-test.txt > pos-test-with-tags.txt
+
+------------
+Second step:
+------------
+$ python scorer.py pos-test-with-tags.txt pos-test-key.txt > pos-tagging-report.txt
+===========
+
+Usage:
+------
+The tagger will take in a training file and a testing file.
+The tagger will then generate a most likely probability tag for each word
+and use some rules to predict the tags for the test file.
+It will then output all the predictions to the output file specified
+
+**********
+Algorithm:
+**********
+
+1. Read in the file paths for the training and testing files
+
+Training:
+---------
+1. Read in the training file
+2. Preprocess unnecessary things out of the line and split into tokens
+3. For each token keep track of how many times the word occurs overall and how many
+    times the tag occurs with that specific word
+4. Loop over all words in the dictionary and calculate the probability of that tag occuring
+    with the word
+5. Get the highest probability tag and assign it to that word as the most likely tag
+
+
+Testing:
+--------
+1. Read in the training file
+2. Preprocess unnecessary things out of the line and split into tokens
+3. For each token check if the word occurs in the probability dictionary
+    3.1 If the word does occur then use the tag associated with that word
+    3.2 If the word does not occur then use the '/NN' tag as default
+4. Loop through all predicted tokens again to optimize using rules
+5. Check each token with the previous word and next word and update tag if necessary
+6. Output predictions using loop
+'''
+
 import re
 import sys
 
-def metaInfo():
-    print("Author: Vinit Patel")
-    print("Class: CMSC-416-001-2021Spring")
-    print("Date: 03/16/2021")
-    print("Tagger and Scorer")
-    print("---------------------------------------------------")
-
 
 def process_line(line):
-    line = line.strip().lower()
+    # Eliminate whitespaces, newlines, and carriage returns
+    line = line.strip()
+
+    # Eliminate phrase brackets and return list of each token
     return re.sub(r'[\[\]]', '', line).split()
 
 
 def add_word_to_wt_dict(word_tag_dict, word, tag):
+    # If the word and tag are in the dictionary, add 1 to the count
     if word in word_tag_dict:
         if tag in word_tag_dict[word]:
             word_tag_dict[word][tag] += 1
+        # Else if the tag has not been used for that word yet
+        # Add the tag to the dictionary and give value of 1
         else:
             word_tag_dict[word][tag] = 1
+    # Else if the word does not occur in the dictionary
+    # Add the word and the tag to the dictionary and give a value of 1
     else:
         word_tag_dict[word] = {tag: 1}
-
-    # print(f'Word: {word}, Tag: {tag}, Count: {word_tag_dict[word][tag]}')
     
+    # Return the modified dictionary
     return word_tag_dict
 
 def add_word_to_dict(dictionary, word):
+    # If the word is in the dictionary then add 1 to the word count
     if word in dictionary:
         dictionary[word] += 1
+    # Else add word to the dictionary and give value of 1
     else:
         dictionary[word] = 1
 
+    # Return modified dictionary
     return dictionary
 
 def split_tag(word):
+    # Get the last occurence of / in the token
     idx = word.rfind('/')
-    # print(f"Last Index of /: {idx} for {word}")
 
+    # Get the base word without escaped '/'
     base = word[0:idx].replace('\/', '/')
+
+    # Get the first associated tag
     tag = word[idx:].split('|')[0]
 
+    # Return word and tag
     return base, tag
 
 
 def create_probability_dict(wt_dict, word_dict):
+    # Used to store probabilities
     prob_dict = {}
 
+    # Loop through all words in the word dictionary
     for k, v in word_dict.items():
-        # print(f"[DEBUG] Calculating Probabilities for: {k}")
-
+        # Dictionary to hold probabilities for current word
         probs = {}
 
+        # For each tag for the word
+        # Divide the number of times the tag is used for that word by
+        # the number of times the word occurs 
         for tag, count in wt_dict[k].items():
-            # print(f"[DEBUG] Tag: {tag}")
-            # print(f"[DEBUG] Tag Count: {count}")
             probs[tag] = count / v
 
+        # Only get the highest probability tag
         highest_tag = max(probs, key=probs.get) if probs else None
 
+        # Assign the highest probability tag to the current word
         prob_dict[k] = highest_tag
 
-        # print(f"[DEBUG] Word: {k}, Probabilities: {probs}")
-        # print(f"[DEBUG] Highest Probability: {highest_tag}")
-
+    # Return the probability dictionary
     return prob_dict
 
+
 def get_tags(lines):
+    # Used to store how many times a tag appears with each word
     word_tag_dictionary = {}
+    # Used to store how many times a word appears in general
     word_dict = {}
-    tag_dict = {}
+    # Used to store list version of each word and tag in read-in sequential order
+    to_file = []
 
+    # Loop through all lines
     for line in lines:
+
+        # Eliminate spaces, brackets, and split into list
         line = process_line(line)
+
+        # Loop through all words in the line
         for word in line:
-            try:
-                word, tag = split_tag(word)
-            except:
-                print(f"Line: {line}")
-                print(f"{word.split('/')}")
-                exit()
+
+            # Split the token into the word and tag
+            word, tag = split_tag(word)
+
+            # Add the word and tag to dictionary
             word_tag_dictionary = add_word_to_wt_dict(word_tag_dictionary, word, tag)
+
+            # Add the word to the dictionary
             word_dict = add_word_to_dict(word_dict, word)
-            tag_dict = add_word_to_dict(tag_dict, tag)
+
+            # Append the curent word and tag to the output list
+            to_file.append((word, tag))
+
+    # Return dictionaries and list
+    return word_tag_dictionary, word_dict, to_file
 
 
-    return word_tag_dictionary, word_dict, tag_dict
-    # print(word_dict)
+# If there are any digits at all within the word
+# predict a cardinal number
+def rule1(word, prev, after, prediction):
+    # print(word)
+    if any(c.isdigit() for c in word):
+        return '/CD', True
+    return prediction, False
+
+
+# If the previous word was a determiner and the next word is a singular noun
+# the current word is more likely to be a adjective
+def rule2(word, prev, after, prediction):
+    if after == '/NN' and prev == 'DT':
+        return '/JJ', True
+    return prediction, False
+
+
+# If the word ends in 'ly' then predict an adverb
+def rule3(word, prev, after, prediction):
+    if len(word) > 2 and word[-2:] == 'ly':
+        return '/RB', True
+    return prediction, False
+
+
+# If the word ends in 'ing' then predict a gerund verb
+def rule4(word, prev, after, prediction):
+    if len(word) > 3 and word[-3:] == 'ing':
+        return '/VBG', True
+    return prediction, False
+
+
+# If the previous word was 'to' and the prediction is a singular noun or mass,
+# Then predict a base form verb instead because it occurs more often than a singlar noun
+def rule5(word, prev, after, prediction):
+    if prev == '/TO' and prediction == '/NN':
+        return '/VB', True
+    return prediction, False
+
+def processRules(word, prev, after, prediction):
+    # Process all the rules with the current word, previous word,
+    # next word, and the current prediction
+
+    # Use rule 1
+    tag, result = rule1(word, prev, after, prediction)
+
+    # If the rule is used then return new tag
+    if result:
+        return tag
+
+    # Use rule 2
+    tag, result = rule2(word, prev, after, prediction)
+
+    # If the rule is used then return new tag
+    if result:
+        return tag
+
+    # Use rule 3
+    tag, result = rule3(word, prev, after, prediction)
+
+    # If the rule is used then return new tag
+    if result:
+        return tag
+
+    # Use rule 4
+    tag, result = rule4(word, prev, after, prediction)
+
+    # If the rule is used then return new tag
+    if result:
+        return tag
+
+    # Use rule 5
+    tag, result = rule5(word, prev, after, prediction)
+
+    # If the rule is used then return new tag
+    if result:
+        return tag
+
+    # If no rules could be used then stick with old prediction
+    return tag
+
+
+def postProcess(predictions):
+    # Used for the very beginning of the file
+    prev = '[START]'
+    # Get the second tag in our predictions
+    after = predictions[1][1]
+
+    # List used to store final predictions
+    postList = []
+
+    # Loop through all the predictions
+    for idx, (word, prediction) in enumerate(predictions):
+        # Try to use rules for a better prediction
+        newPrediction = processRules(word, prev, after, prediction)
+        
+        # Assign current word to previous word
+        prev = newPrediction
+
+        # If we are at the last element then use [END] tag for after
+        if idx == len(predictions) - 1:
+            after = '[END]'
+        else:
+            # Else we can just use the next tag in our predictions
+            after = predictions[idx + 1][1]
+
+        # Append new prediction to final list
+        postList.append((word, newPrediction))
+
+    # Return final predictions
+    return postList
+
 
 def predict_tags(lines, prob_dict):
-    predictions = {}
+    to_file = []
+    words = []
 
+    # Loop through each line
     for line in lines:
+        # Eliminate spaces, brackets, and split into list
         line = process_line(line)
-        for word in line:
-            word = word.replace('\/', '/')
-            if word not in prob_dict:
-                predictions = add_word_to_wt_dict(predictions, word, '/nn')
-            else:
-                predictions = add_word_to_wt_dict(predictions, word, prob_dict[word])
 
-    return predictions
+        # Loop through each word in the line
+        for word in line:
+            # Replace any escaped forward slashes for clarity
+            word = word.replace('\/', '/')
+
+            # Append to words list
+            words.append(word)
+
+    # Loop through words list
+    for word in words:
+        # If the word is in our training set then use it
+        if word in prob_dict:
+            # Get the most likely tag for that word from the probability dictionary
+            prediction = prob_dict[word]
+        else:
+            # If the word is not in our training set then default to the tag /NN
+            prediction = '/NN'
+
+        # Append to the output list
+        to_file.append((word, prediction))
+
+    # Process all the words again but with additional rules
+    to_file = postProcess(to_file)
+
+    # Return prediction list
+    return to_file
 
 def parse_training_tags(trainFilePath):
 
+    # Read in training file
     with open(trainFilePath, 'r') as f:
         lines = f.readlines()
 
-        return get_tags(lines)
+    # Generate word and tag dictionary with training data 
+    return get_tags(lines)
 
 def parse_test_file(testFilePath, prob_dict):
 
+    # Read in test file
     with open(testFilePath, 'r') as f:
         lines = f.readlines()
 
+    # Predict tags using probability dictionary
     return predict_tags(lines, prob_dict)
 
-# def compare_to_key(keyFilePath, predictions):
-
-#     with open(keyFilePath, 'r'):
-#         lines = f.readlines()
-
-#     wt_dict, word_dict, tag_dict = get_tags(lines)
-
-#     print(wt_dict)
 
 def parseParameters():
     argv = sys.argv
 
+    # Make sure we have exactly 3 arguments
     if len(argv) != 3:
         print("Run Command Example: python tagger.py pos-train.txt pos-test.txt > pos-test-with-tags.txt")
         sys.exit(1)
 
+    # Return training and testing file paths
     return argv[1], argv[2]
 
 def main():
-    #metaInfo()
+    # Get the file path for the training and test dataset
     trainFilePath, testFilePath = parseParameters()
-    wt_dict, word_dict, tag_dict = parse_training_tags(trainFilePath)
+
+    # Parse the training file and generate the dictionary of words and tags
+    wt_dict, word_dict, to_file = parse_training_tags(trainFilePath)
+
+    # Calculate the most likely tags
     prob_dict = create_probability_dict(wt_dict, word_dict)
-    predictions = parse_test_file(testFilePath, prob_dict)
 
-    print(predictions)
+    # Parse and predict the test file
+    to_file = parse_test_file(testFilePath, prob_dict)
 
+    # Output the predictions
+    for prediction in to_file:
+        print(f'{prediction[0]}{prediction[1]}')
 
 if __name__ == '__main__':
     main()
